616eed257ee03a360a982db938c48a0308526774
Jose Rostagno <joserostagno@hotmail.com>
2016-11-27 23:25:08 -0300

    Agregar licencia a los archivos

--- NetworkManager/NetworkManagerHelperGio.py
+++ NetworkManager/NetworkManagerHelperGio.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-#    This file is part of emesene.
+#    This file is part of escucharTweets.
 #
-#    emesene is free software; you can redistribute it and/or modify
+#    escucharTweets is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation; either version 3 of the License, or
 #    (at your option) any later version.
@@ -13,7 +13,7 @@
 #    GNU General Public License for more details.
 #
 #    You should have received a copy of the GNU General Public License
-#    along with emesene; if not, write to the Free Software
+#    along with escucharTweets; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 import gi
